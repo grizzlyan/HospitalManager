e5e29e1a1c751f887cec827bfad14fa50a2b76f0
Grizzlyan <grizzlyan140@gmail.com>
2021-12-07 17:15:53 +0200

    added specializations description

--- HospitalManager/HospitalManager.Data/DataAccess/Configs/SpecializationConfiguration.cs
+++ HospitalManager/HospitalManager.Data/DataAccess/Configs/SpecializationConfiguration.cs
@@ -17,33 +17,143 @@ namespace HospitalManager.Data.DataAccess.Configs
 
             builder.HasData(
                 new Specialization
-                { 
-                    Id = 1, 
-                    SpecializationName = "Проктология", 
+                {
+                    Id = 1,
+                    SpecializationName = "Проктология",
                     Description = "Лечение геморроя и других проктологических заболеваний с помощью малоинвазивных методик."
                 },
-                new Specialization { Id = 2, SpecializationName = "Урология" },
-                new Specialization { Id = 3, SpecializationName = "Гинекология" },
-                new Specialization { Id = 4, SpecializationName = "Дерматология" },
-                new Specialization { Id = 5, SpecializationName = "Гастроэнтерология" },
-                new Specialization { Id = 6, SpecializationName = "Эндокринология" },
-                new Specialization { Id = 7, SpecializationName = "Кардиология" },
-                new Specialization { Id = 8, SpecializationName = "Отоларингология" },
-                new Specialization { Id = 9, SpecializationName = "Неврология" },
-                new Specialization { Id = 10, SpecializationName = "Маммология" },
-                new Specialization { Id = 11, SpecializationName = "Хирургия" },
-                new Specialization { Id = 12, SpecializationName = "Флебология" },
-                new Specialization { Id = 13, SpecializationName = "Пульмонология" },
-                new Specialization { Id = 14, SpecializationName = "Аллергология" },
-                new Specialization { Id = 15, SpecializationName = "Травматология" },
-                new Specialization { Id = 16, SpecializationName = "Инфектология" },
-                new Specialization { Id = 17, SpecializationName = "Офтальмология" },
-                new Specialization { Id = 18, SpecializationName = "Нефрология" },
-                new Specialization { Id = 19, SpecializationName = "Гематология" },
-                new Specialization { Id = 20, SpecializationName = "Ревматология" },
-                new Specialization { Id = 21, SpecializationName = "Психотерапия" },
-                new Specialization { Id = 22, SpecializationName = "Пластическая хирургия" },
-                new Specialization { Id = 23, SpecializationName = "Нейрохирургия" });
+                new Specialization
+                {
+                    Id = 2,
+                    SpecializationName = "Урология",
+                    Description = "Мужская и женская урология.Лечение проблем с потенцией.Консультация андролога."
+                },
+                new Specialization
+                {
+                    Id = 3,
+                    SpecializationName = "Гинекология",
+                    Description = "Диагностика и лечение патологий шейки матки, воспалительных и невоспалительных заболеваний."
+                },
+                new Specialization
+                {
+                    Id = 4,
+                    SpecializationName = "Дерматология",
+                    Description = "Диагностика и лечение дерматологических заболеваний. Удаление кожных новообразований."
+                },
+                new Specialization
+                {
+                    Id = 5,
+                    SpecializationName = "Гастроэнтерология",
+                    Description = "Эффективное лечение заболеваний желудочно-кишечного тракта. Консультации по вопросам питания."
+                },
+                new Specialization
+                {
+                    Id = 6,
+                    SpecializationName = "Эндокринология",
+                    Description = "Диагностика и лечение заболеваний эндокринной системы: ожирения, сахарного диабета и щитовидной железы."
+                },
+                new Specialization
+                {
+                    Id = 7,
+                    SpecializationName = "Кардиология",
+                    Description = "Лечение заболеваний сердечно-сосудистой системы человека. Проведение ЭКГ, УЗИ сердца."
+                },
+                new Specialization
+                {
+                    Id = 8,
+                    SpecializationName = "Отоларингология",
+                    Description = "Диагностика и лечение заболеваний ЛОР-органов (уши, горло, нос). Удаление инородных предметов и серных пробок."
+                },
+                new Specialization
+                {
+                    Id = 9,
+                    SpecializationName = "Неврология",
+                    Description = "Диагностика и лечение заболеваний нервной системы человека. Лечение нарушений слуха, речи, памяти."
+                },
+                new Specialization
+                {
+                    Id = 10,
+                    SpecializationName = "Маммология",
+                    Description = "Диагностика и лечение заболеваний молочных желез. Пункция, УЗИ. Консультации маммолога-онколога."
+                },
+                new Specialization
+                {
+                    Id = 11,
+                    SpecializationName = "Хирургия",
+                    Description = "Удаление доброкачественных подкожных новообразований малоинвазивными методами. Консультации хирурга."
+                },
+                new Specialization
+                {
+                    Id = 12,
+                    SpecializationName = "Флебология",
+                    Description = "Диагностика и лечение заболеваний вен (атеросклероз, тромбоз, заболеваний аорты). Лечение варикоза."
+                },
+                new Specialization
+                {
+                    Id = 13,
+                    SpecializationName = "Пульмонология",
+                    Description = "Диагностика и лечение заболеваний органов дыхания (легкие, бронхи, трахеи). Лечение последствий курения."
+                },
+                new Specialization
+                {
+                    Id = 14,
+                    SpecializationName = "Аллергология",
+                    Description = "Диагностика, лечение и профилактика аллергии различного происхождения. Анализы на аллергены."
+                },
+                new Specialization
+                {
+                    Id = 15,
+                    SpecializationName = "Травматология",
+                    Description = "Лечение заболеваний опорно-двигательного аппарата. Укол-блокады, prp-терапия."
+                },
+                new Specialization
+                {
+                    Id = 16,
+                    SpecializationName = "Инфектология",
+                    Description = "Диагностика, лечение и профилактика инфекционных заболеваний."
+                },
+                new Specialization
+                {
+                    Id = 17,
+                    SpecializationName = "Офтальмология",
+                    Description = "Диагностика и лечение нарушений зрения у взрослых и детей. Подбор средств для зрения."
+                },
+                new Specialization
+                {
+                    Id = 18,
+                    SpecializationName = "Нефрология",
+                    Description = "Диагностика и лечение заболеваний почек: мочекаменная болезнь, пиелонефрит, хроническая болезнь почек."
+                },
+                new Specialization
+                {
+                    Id = 19,
+                    SpecializationName = "Гематология",
+                    Description = "Диагностика и лечение различных заболеваний крови и системы кроветворения."
+                },
+                new Specialization
+                {
+                    Id = 20,
+                    SpecializationName = "Ревматология",
+                    Description = "Лечение и профилактика заболеваний суставов и соединительной ткани."
+                },
+                new Specialization
+                {
+                    Id = 21,
+                    SpecializationName = "Психотерапия",
+                    Description = "Диагностика и лечение пациентов с острыми и тяжелыми психическими состояниями."
+                },
+                new Specialization
+                {
+                    Id = 22,
+                    SpecializationName = "Пластическая хирургия",
+                    Description = "Устранение косметических дефектов, а также деформаций тканей и органов."
+                },
+                new Specialization
+                {
+                    Id = 23,
+                    SpecializationName = "Нейрохирургия",
+                    Description = "Оперативное лечение заболеваний центральной и периферической нервной системы."
+                });
 
             /*builder.HasData(
                 new MedicalProfession { Id = 1, ProfessionName = "Proctology" },
